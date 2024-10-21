ee04f0157c225443413e1168d37864c93d229e2a
Cheikh Ahmed tidiane <cheikhtidiane15.thiam@ucad.edu.sn>
2024-10-21 21:51:26 +0000

    n

---
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NewTicketComponent } from "../new-ticket/new-ticket.component";
+
+@Component({
+  selector: 'app-ticket',
+  standalone: true,
+  imports: [],
+  templateUrl: './ticket.component.html',
+  styleUrl: './ticket.component.css'
+})
+export class TicketComponent {
+
+}
