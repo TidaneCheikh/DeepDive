6a0b324d2e6c4d96dc786561df94a9b74b3874d3
cheikhtidiane <cheikhtidiane15.thiam@ucad.edu.sn>
2024-10-24 12:46:47 +0000

    the status

--- src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-item',
   standalone: true,
   imports: [],
   templateUrl: './dashboard-item.component.html',
-  styleUrl: './dashboard-item.component.css'
+  styleUrl: './dashboard-item.component.css',
+  //encapsulation:ViewEncapsulation.None,
 })
 export class DashboardItemComponent {
   @Input() image !: { scr :string ,alt:string}  ;
